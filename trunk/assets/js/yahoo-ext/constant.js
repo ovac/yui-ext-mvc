a33524026de577a67aed1ed3a6229d88dd7caba3
mattesnider <mattesnider@84ebbab2-1041-11de-b0c5-ff504ef5068a>
2009-08-27 18:09:36 +0000

    Library improvements

--- trunk/assets/js/yahoo-ext/constant.js
+++ trunk/assets/js/yahoo-ext/constant.js
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2009, Matt Snider, LLC All rights reserved.
- * Version: 1.0.03
+ * Version: 1.0.04
  */
 
 /**
@@ -8,13 +8,19 @@
  * @module window
  */
 (function() {
+	var YL = YAHOO.lang, CLS = {};
     
     /**
      * This Class contains global constants that to made be available throughout the codebase.
      * @class C
      * @static
      */
-    if (! YAHOO.lang.isObject(window.C)) {window.C = {};} // don't override C, unless necessary
+    if (! YL.isObject(window.C)) {window.C = {};} // don't override C, unless necessary
+	YL.augmentObject(window, {
+		F: false,
+		N: null,
+		T: true
+	});
 
     /**
      * The global object to hold all HTML constants.
@@ -24,143 +30,145 @@
      */
     C.HTML={};
 
+	YL.augmentObject(CLS, {
+		/**
+		 * The DOM class attribute for applying "disabled" styles and/or identifying element state.
+		 * @property DISABLED
+		 * @type {String}
+		 * @static
+		 * @final
+		 * @readonly Defined at build time.
+		 */
+		DISABLED:'disabled',
+
+		/**
+		 * The DOM class attribute for applying "error" styles.
+		 * @property ERROR
+		 * @type {String}
+		 * @static
+		 * @final
+		 * @readonly Defined at build time.
+		 */
+		ERROR:'error',
+
+		/**
+		 * The DOM class attribute for emulating ":first-child" psuedo class.
+		 * @property FIRST
+		 * @type {String}
+		 * @static
+		 * @final
+		 * @readonly Defined at build time.
+		 */
+		FIRST:'first',
+
+		/**
+		 * The DOM class attribute for applying the "visibility:hidden" style.
+		 * @property HIDDEN
+		 * @type {String}
+		 * @static
+		 * @final
+		 * @readonly Defined at build time.
+		 */
+		HIDDEN:'hidden',
+
+		/**
+		 * The DOM class attribute for applying the "display:none" style.
+		 * @property HIDE
+		 * @type {String}
+		 * @static
+		 * @final
+		 * @readonly Defined at build time.
+		 */
+		HIDE:'displayNone',
+
+		/**
+		 * The DOM class attribute for emulating ":hover" psuedo class.
+		 * @property HOVER
+		 * @type {String}
+		 * @static
+		 * @final
+		 * @readonly Defined at build time.
+		 */
+		HOVER:'hover',
+
+		/**
+		 * The DOM class attribute for emulating ":last-child" psuedo class.
+		 * @property LAST
+		 * @type {String}
+		 * @static
+		 * @final
+		 * @readonly Defined at build time.
+		 */
+		LAST:'last',
+
+		/**
+		 * The DOM class attribute for applying "masked" styles.
+		 * @property MASKED
+		 * @type {String}
+		 * @static
+		 * @final
+		 * @readonly Defined at build time.
+		 */
+		MASKED:'masked',
+
+		/**
+		 * The DOM class attribute for applying "message" styles.
+		 * @property MESSAGE
+		 * @type {String}
+		 * @static
+		 * @final
+		 * @readonly Defined at build time.
+		 */
+		MESSAGE:'message',
+
+		/**
+		 * The DOM class attribute for identifying "next" elements (usually used in pagination).
+		 * @property NEXT
+		 * @type {String}
+		 * @static
+		 * @final
+		 * @readonly Defined at build time.
+		 */
+		NEXT:'next',
+
+		/**
+		 * The DOM class attribute for applying "open" styles and/or identifying element state.
+		 * @property OPEN
+		 * @type {String}
+		 * @static
+		 * @final
+		 * @readonly Defined at build time.
+		 */
+		OPEN:'open',
+
+		/**
+		 * The DOM class attribute for identifying "previous" elements (usually used in pagination).
+		 * @property PREV
+		 * @type {String}
+		 * @static
+		 * @final
+		 * @readonly Defined at build time.
+		 */
+		PREV:'prev',
+
+		/**
+		 * The DOM class attribute for applying "selected" styles and/or identifying element state.
+		 * @property SELECTED
+		 * @type {String}
+		 * @static
+		 * @final
+		 * @readonly Defined at build time.
+		 */
+		SELECTED:'selected'
+	});
+
     /**
      * The global object to hold all className constants.
      * @namespace C.HTML
      * @class CLS
      * @static
      */
-    C.HTML.CLS={};
-
-    /**
-     * The DOM class attribute for applying "disabled" styles and/or identifying element state.
-     * @property DISABLED
-     * @type {String}
-     * @static
-     * @final
-     * @readonly Defined at build time.
-     */
-    C.HTML.CLS.DISABLED='disabled';
-
-    /**
-     * The DOM class attribute for applying "error" styles.
-     * @property ERROR
-     * @type {String}
-     * @static
-     * @final
-     * @readonly Defined at build time.
-     */
-    C.HTML.CLS.ERROR='error';
-
-    /**
-     * The DOM class attribute for emulating ":first-child" psuedo class.
-     * @property FIRST
-     * @type {String}
-     * @static
-     * @final
-     * @readonly Defined at build time.
-     */
-    C.HTML.CLS.FIRST='first';
-
-    /**
-     * The DOM class attribute for applying the "visibility:hidden" style.
-     * @property HIDDEN
-     * @type {String}
-     * @static
-     * @final
-     * @readonly Defined at build time.
-     */
-    C.HTML.CLS.HIDDEN='hidden';
-
-    /**
-     * The DOM class attribute for applying the "display:none" style.
-     * @property HIDE
-     * @type {String}
-     * @static
-     * @final
-     * @readonly Defined at build time.
-     */
-    C.HTML.CLS.HIDE='displayNone';
-
-    /**
-     * The DOM class attribute for emulating ":hover" psuedo class.
-     * @property HOVER
-     * @type {String}
-     * @static
-     * @final
-     * @readonly Defined at build time.
-     */
-    C.HTML.CLS.HOVER='hover';
-
-    /**
-     * The DOM class attribute for emulating ":last-child" psuedo class.
-     * @property LAST
-     * @type {String}
-     * @static
-     * @final
-     * @readonly Defined at build time.
-     */
-    C.HTML.CLS.LAST='last';
-
-    /**
-     * The DOM class attribute for applying "masked" styles.
-     * @property MASKED
-     * @type {String}
-     * @static
-     * @final
-     * @readonly Defined at build time.
-     */
-    C.HTML.CLS.MASKED='masked';
-
-    /**
-     * The DOM class attribute for applying "message" styles.
-     * @property MESSAGE
-     * @type {String}
-     * @static
-     * @final
-     * @readonly Defined at build time.
-     */
-    C.HTML.CLS.MESSAGE='message';
-
-    /**
-     * The DOM class attribute for identifying "next" elements (usually used in pagination).
-     * @property NEXT
-     * @type {String}
-     * @static
-     * @final
-     * @readonly Defined at build time.
-     */
-    C.HTML.CLS.NEXT='next';
-
-    /**
-     * The DOM class attribute for applying "open" styles and/or identifying element state.
-     * @property OPEN
-     * @type {String}
-     * @static
-     * @final
-     * @readonly Defined at build time.
-     */
-    C.HTML.CLS.OPEN='open';
-
-    /**
-     * The DOM class attribute for identifying "previous" elements (usually used in pagination).
-     * @property PREV
-     * @type {String}
-     * @static
-     * @final
-     * @readonly Defined at build time.
-     */
-    C.HTML.CLS.PREV='prev';
-
-    /**
-     * The DOM class attribute for applying "selected" styles and/or identifying element state.
-     * @property SELECTED
-     * @type {String}
-     * @static
-     * @final
-     * @readonly Defined at build time.
-     */
-    C.HTML.CLS.SELECTED='selected';
+	C.HTML.CLS = CLS;
 
     /**
      * The global object to hold all ID constants.
@@ -197,4 +205,4 @@
      * @readonly Defined at build time.
      */
     C.HTML.NAME.TASK='task';
-})();
+}());
